668411e91efcc3ce8b8e58e216793417bbb9989a
Alexander Lee <leeajl006@gmail.com>
2023-11-17 18:11:38 -0500

    Update table

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import TableComponent from '../components/TableComponent';
 
 const Crypto = () => {
-  return <div>Crypto</div>;
+  return (
+    <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+      <TableComponent />
+    </section>
+  );
 };
 
 export default Crypto;
